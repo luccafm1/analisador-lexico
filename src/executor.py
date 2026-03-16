1b4f6978dc654217af58f1a6d5fef76f93354b51
Gabiru1089 <gabriel1m52020@gmail.com>
2026-03-16 18:44:51 -0300

    feat: adicionando token e encontrar aninhamento

--- src/executor.py
+++ src/executor.py
@@ -14,3 +14,89 @@ def testar_executor():
     Deve cobrir operações simples, aninhadas e comandos RES/MEM.
     """
     pass
+from lexer import parseExpressao, LexError
+
+"""
+executarExpressao.py
+
+Avalia as expressões produzidas em lexer.py
+
+Formato do Token esperado: tupla (tipo, valor, posição)
+    ("NUM", "3.14", 2)
+    ("OP", "+", 7)
+    ("LPAREN", "(", 0)
+    ("RPAREN", ")", 9)
+    ("RES", "RES", 5)
+    ("MEM", "VAR", 3)
+
+Depende de:
+    lexer.py -> parseExpressao, LexError
+
+"""
+
+# Constantes - tipos de token do lexer
+
+T_NUM = "NUM"
+T_OP = "OP"
+T_LPAREN = "LPAREN"
+T_RPAREN = "RPAREN"
+T_RES ="RES"
+T_MEM = "MEM"
+
+
+# Funções para auxiliar na identificaçãomdos itens do token
+
+def tipo(token: tuple) -> str:
+    return token[0]
+
+def valor(token: tuple) -> str:
+    return token[1]
+
+def index(token: tuple) -> int:
+    return token[2]
+
+
+# Exceções específicas de execução
+
+class ErroDivisaoPorZero(Exception):
+    pass
+
+class ErroExpressaoInvalida(Exception):
+    pass
+
+class ErroMemoriaNaoInicializada(Exception):
+    pass
+class ErroHistoricoInvalido(Exception):
+    pass
+
+#Converter linha em tokens utilizando o lexer
+
+def tokenizar(linha : str) -> str:
+    """Chama a parseExpressao do Lexer e retorna lista de de tokens
+        propaga LexError se tiverem símbolos inválidos
+    """
+
+    tokens = []
+    parseExpressao(linha, tokens)
+    return tokens
+
+
+def encontrar_fechamento(tokens: list, inicio: int) -> int:
+    """
+    tokens[inicio] é um LPAREN, a função retorna o RPAREN
+    correspondente (mesmo nível de profundidade)
+    """
+    profundidade = 0
+    i = inicio
+
+    while i < len(tokens):
+        if tipo(tokens[i]) == T_LPAREN:
+            profundidade += 1
+        elif tipo(tokens[i]) == T_RPAREN:
+            profundidade -= 1
+            if profundidade == 0:
+                return i
+        i += 1
+    raise ErroExpressaoInvalida(
+        f"Parêntese aberto na posição {tokens[inicio]} não foi fechado!"
+    )
